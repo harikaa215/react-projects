c3632356cd0b64f4cd908f21e32f228b6c97d7ae
Harika A <msgtoharika@gmail.com>
2024-04-07 19:49:51 +0530

    fixed delivery time

--- my-food-app/src/components/Restaurant.jsx
+++ my-food-app/src/components/Restaurant.jsx
@@ -2,14 +2,13 @@ import { IMG_URL } from "../utils/constants";
 
 const Restaurant = ({resData}) => {
 
-  const {name, avgRating, cloudinaryImageId, costForTwo, cuisines} = resData.info;
-  const {slaString} = resData.info.sla;
+  const {name, avgRating, cloudinaryImageId, costForTwo, cuisines, sla} = resData.info;
 
   return (
     <div className="restaurant_card">
         <img src ={IMG_URL+cloudinaryImageId}/>
         <h4>{name}</h4>
-        <p className="ratings">⭐{avgRating}.  <span>{slaString}</span>  <span>{costForTwo}</span></p>
+        <p className="ratings">⭐{avgRating}.  <span>{sla.slaString}</span>  <span>{costForTwo}</span></p>
         <p className="cuisines">{cuisines.join(", ")}</p>
     </div>
   )
